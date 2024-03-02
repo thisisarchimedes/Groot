53b70311211a269d40db55e797f01e851d3b1acc
tomerXarchimedes <tomer@archimedesfi.com>
2024-03-02 13:15:37 -0700

    Update LocalHardhatNode.ts

--- src/LocalHardhatNode.ts
+++ src/LocalHardhatNode.ts
@@ -1,9 +1,11 @@
 import { Web3 } from 'web3';
 
 export class LocalHardhatNode {
-    private readonly web3: Web3;
+    private web3: Web3;
+    private rpcUrl: string;
 
     constructor(rpc_url: string) {
+        this.rpcUrl = rpc_url;
         this.web3 = new Web3(rpc_url);
     }
 
@@ -14,4 +16,51 @@ export class LocalHardhatNode {
         
         return blockNumber;
     }
+
+    public async resetHardhatNode() {
+
+        await this.sendJsonRpcRequest(this.rpcUrl, 'hardhat_reset', [{
+                forking: {
+                    jsonRpcUrl: 'https://eth-mainnet.g.alchemy.com/v2/pPpHjtxSDGQxVHCjatsahkR0Ie79w_Qu',
+                },
+            }] as any); 
+
+        while (true) {
+            if (await this.isNodeReady()) {
+                console.log('Node is ready.');
+                break;
+            } else {
+                console.log('Waiting for the node to be ready...');
+                await new Promise(resolve => setTimeout(resolve, 1000)); // Wait for 1 second before retrying
+            }
+            }
+    }
+
+    private async  sendJsonRpcRequest(url: string, method: string, params = []) {
+        const response = await fetch(url, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            jsonrpc: '2.0',
+            method: method,
+            params: params,
+            id: new Date().getTime() // Use timestamp for a unique ID
+          }),
+        });
+      
+        if (!response.ok) {
+          throw new Error(`HTTP error! status: ${response.status}`);
+        }
+      
+        return response.json();
+      }
+
+      private async isNodeReady() {
+        try {
+          await this.sendJsonRpcRequest(this.rpcUrl, 'eth_chainId');
+          return true; // Node responded successfully
+        } catch (error) {
+          return false; // Node response indicated not ready
+        }
+      }
 }
