39941772ff000626bb1b4f41f7bd43627e2d4341
tomerXarchimedes <tomer@archimedesfi.com>
2024-03-02 09:31:45 -0700

    update

---
+++ test/app.spec.ts
@@ -0,0 +1,7 @@
+import { expect } from 'chai';
+
+describe('Initial Test', function () {
+    it('should pass', function () {
+        expect(true).to.be.true;
+    });
+});
--- test/environment.test.ts
+++
@@ -1,7 +0,0 @@
-import { expect } from "chai";
-
-describe("Environment setup", function () {
-  it("Should get current block number from local Hardhat node", async function () {
-
-  });
-});
