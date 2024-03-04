b120a99f3246cc6f07258d634cd27e8ef9230c02
tomerXarchimedes <tomer@archimedesfi.com>
2024-03-04 16:18:44 -0500

    update

--- test/docker/LocalHardhatNodeControl.test.ts
+++ test/docker/LocalHardhatNodeControl.test.ts
@@ -2,29 +2,29 @@ import * as chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import * as dotenv from 'dotenv';
 
-import {LocalNodeHardhat, LocalNodeError} from '../../src/LocalNodeHardhat';
+import { LocalNodeHardhat, LocalNodeError } from '../../src/blockchain_reader/LocalNodeHardhat';
 
 dotenv.config();
 chai.use(chaiAsPromised);
 
-const {expect} = chai;
+const { expect } = chai;
 
-describe('Check that we work with local Hardhat node correctly', function() {
+describe('Check that we work with local Hardhat node correctly', function () {
   // eslint-disable-next-line no-invalid-this
   this.timeout(120000);
 
   let localNode: LocalNodeHardhat;
 
-  beforeEach(async function() {
+  beforeEach(async function () {
     localNode = new LocalNodeHardhat(8545, 'archimedes-node-alchemy');
     await localNode.startNode();
   });
 
-  afterEach(async function() {
+  afterEach(async function () {
     await localNode.stopNode();
   });
 
-  it('Should be able to reset node and point it to invalid RPC', async function() {
+  it('Should be able to reset node and point it to invalid RPC', async function () {
     try {
       await localNode.resetNode('invalidRPCUrl');
       expect.fail('Expected resetNode to throw an error');
@@ -33,7 +33,7 @@ describe('Check that we work with local Hardhat node correctly', function() {
     }
   });
 
-  it('Should be able to reset node and point it to valid RPC', async function() {
+  it('Should be able to reset node and point it to valid RPC', async function () {
     const alchemyRpcUrl: string = 'https://eth-mainnet.g.alchemy.com/v2/' + process.env.API_KEY_ALCHEMY;
     await resetAndVerify(alchemyRpcUrl);
 
@@ -48,9 +48,9 @@ describe('Check that we work with local Hardhat node correctly', function() {
     expect(blockNumber > 1934000n).to.be.true;
   }
 
-  it('Should be able to invoke aribtrary view on-chain method', async function() {
+  it('Should be able to invoke aribtrary view on-chain method', async function () {
     // eslint-disable-next-line max-len
-    const abi = [{'inputs': [], 'name': 'decimals', 'outputs': [{'internalType': 'uint8', 'name': '', 'type': 'uint8'}], 'stateMutability': 'view', 'type': 'function'}];
+    const abi = [{ 'inputs': [], 'name': 'decimals', 'outputs': [{ 'internalType': 'uint8', 'name': '', 'type': 'uint8' }], 'stateMutability': 'view', 'type': 'function' }];
     const usdcContractAddress: string = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
 
     const res = Number(await localNode.callViewFunction(usdcContractAddress, abi, 'decimals'));
