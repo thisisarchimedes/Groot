947b4c8b8a417c435bc66943bc2d1c8f189873dc
aliberkhsl <aliberkhasil@gmail.com>
2024-03-12 18:33:09 +0300

    uni_rebalance

--- src/rule_engine/rule/RuleUniswapPSPRebalance.ts
+++ src/rule_engine/rule/RuleUniswapPSPRebalance.ts
@@ -1,15 +1,15 @@
-import { BlockchainReader } from '../../blockchain/blockchain_reader/BlockchainReader';
-import { Logger } from '../../service/logger/Logger';
-import { ToolStrategyUniswap } from '../tool/ToolStrategyUniswap';
+import {BlockchainReader} from '../../blockchain/blockchain_reader/BlockchainReader';
+import {Logger} from '../../service/logger/Logger';
+import {ToolStrategyUniswap} from '../tool/ToolStrategyUniswap';
 import {Rule, RuleParams} from './Rule';
-
+// import {UNISWAPV3_POOL_ABI} from '@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json';
 /* eslint-disable max-len */
 export interface RuleParamsUniswapPSPRebalance extends RuleParams {
-    pperTriggerThresholdPercentage: number, // trigger rebalance when we are more than (upperThreshold% * upper tick)
-    lowerTriggerThresholdPercentage: number, // trigger rebalance when we are less than (lowerThreshold% * lower tick)
-    upperTargetTickPercentage: number, // Where we want to be after rebalance currentUniswapTick * newUpperTickPercentage = newUpperTick
-    lowerTargetTickPercentage: number, // Where we want to be after rebalance currentUniswapTick * newLowerTickPercentage = newLowerTick
-    strategyAddress: string,
+  pperTriggerThresholdPercentage: number; // trigger rebalance when we are more than (upperThreshold% * upper tick)
+  lowerTriggerThresholdPercentage: number; // trigger rebalance when we are less than (lowerThreshold% * lower tick)
+  upperTargetTickPercentage: number; // Where we want to be after rebalance currentUniswapTick * newUpperTickPercentage = newUpperTick
+  lowerTargetTickPercentage: number; // Where we want to be after rebalance currentUniswapTick * newLowerTickPercentage = newLowerTick
+  strategyAddress: string;
 }
 /* eslint-enable max-len */
 
@@ -22,22 +22,26 @@ export class RuleUniswapPSPRebalance extends Rule {
       params: RuleParams,
   ) {
     super(logger, blockchainReader, ruleLabel, params);
-    this.uniswapStrategy = new ToolStrategyUniswap((params as RuleParamsUniswapPSPRebalance).strategyAddress, blockchainReader);
-
+    this.uniswapStrategy = new ToolStrategyUniswap(
+        (params as RuleParamsUniswapPSPRebalance).strategyAddress,
+        blockchainReader,
+    );
   }
   public async evaluate(): Promise<void> {
-    // fetch the pool address from strategy
-    // fetch the currentTick from uniswap pool
-    // fetch the upper and lower tick from strategy
     // check if based on the thresholds if we are in proper range then no action
     // if not calculate new lowertick and uppertick based on the currentTick
     // call rebalance function based on the new params
-
-    const pool = await this.uniswapStrategy.getPoolAddress();
+    // const pool = await this.uniswapStrategy.getPoolAddress();
+    // const currentTick = await this.blockchainReader.callViewFunction(
+    //     pool,
+    //     UNISWAPV3_POOL_ABI,
+    //     'slot0',
+    // );
+    // const upperTick = await this.uniswapStrategy.upperTick();
+    // const lowerTick = await this.uniswapStrategy.lowerTick();
   }
 
   protected generateUniqueKey(): string {
     throw new Error('Method not implemented.');
   }
 }
-
--- src/rule_engine/tool/ToolStrategyUniswap.ts
+++ src/rule_engine/tool/ToolStrategyUniswap.ts
@@ -1,20 +1,38 @@
-import { BlockchainReader } from '../../blockchain/blockchain_reader/BlockchainReader';
-import { Logger } from '../../service/logger/Logger';
+import {BlockchainReader} from '../../blockchain/blockchain_reader/BlockchainReader';
+import {Logger} from '../../service/logger/Logger';
 import UNISWAPV3_STRATEGY_ABI from '../../constants/abis/UNISWAPV3_STRATEGY_ABI.json';
 
 export class ToolStrategyUniswap {
-    private readonly strategyAddress: string;
-    private readonly blockchainReader: BlockchainReader;
+  private readonly strategyAddress: string;
+  private readonly blockchainReader: BlockchainReader;
 
-    constructor(strategyAddress: string, blockchainReader: BlockchainReader) {
-        this.strategyAddress = strategyAddress;
-        this.blockchainReader = blockchainReader;
-    }
+  constructor(strategyAddress: string, blockchainReader: BlockchainReader) {
+    this.strategyAddress = strategyAddress;
+    this.blockchainReader = blockchainReader;
+  }
 
-    public async getPoolAddress(): Promise<string> {
-        const ret = await this.blockchainReader.callViewFunction(this.strategyAddress, UNISWAPV3_STRATEGY_ABI, 'pool');
-        return ret as string;
-    }
+  public async getPoolAddress(): Promise<string> {
+    const ret = await this.blockchainReader.callViewFunction(
+        this.strategyAddress,
+        UNISWAPV3_STRATEGY_ABI,
+        'pool',
+    );
+    return ret as string;
+  }
+  public async upperTick(): Promise<number> {
+    const ret = await this.blockchainReader.callViewFunction(
+        this.strategyAddress,
+        UNISWAPV3_STRATEGY_ABI,
+        'upperTick',
+    );
+    return ret as number;
+  }
+  public async lowerTick(): Promise<number> {
+    const ret = await this.blockchainReader.callViewFunction(
+        this.strategyAddress,
+        UNISWAPV3_STRATEGY_ABI,
+        'lowerTick',
+    );
+    return ret as number;
+  }
 }
-
-
