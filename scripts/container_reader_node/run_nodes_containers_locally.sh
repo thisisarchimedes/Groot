885477975edda3b1550c1fdcc1cfd323beea4111
tomerXarchimedes <tomer@archimedesfi.com>
2024-03-17 21:58:44 -0400

    Update run_nodes_containers_locally.sh

--- scripts/container_reader_node/run_nodes_containers_locally.sh
+++ scripts/container_reader_node/run_nodes_containers_locally.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-docker run -d --rm --name main-eth-node -p 8545:8545 arch-production-node:latest
-docker run -d --rm --name alt-eth-node -p 8546:8545 arch-production-node:latest
+docker run -e HARDHAT_PORT=8545 -d --rm --name main-eth-node -p 8545:8545 arch-production-node:latest
+docker run -e HARDHAT_PORT=18545 -d --rm --name alt-eth-node -p 18545:18545 arch-production-node:latest
