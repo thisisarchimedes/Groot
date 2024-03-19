e24ada1ceb32936794f5d55b4c8b7105e2a0d7e6
aliberkhsl <aliberkhasil@gmail.com>
2024-03-19 16:20:03 +0300

    add : uni rule

--- src/rule_engine/rule/RuleUniswapPSPRebalance.ts
+++ src/rule_engine/rule/RuleUniswapPSPRebalance.ts
@@ -1,11 +1,11 @@
-import {BlockchainReader} from '../../blockchain/blockchain_reader/BlockchainReader';
-import {Logger} from '../../service/logger/Logger';
-import {ToolStrategyUniswap} from '../tool/ToolStrategyUniswap';
-import {Rule, RuleParams} from './Rule';
+import { BlockchainReader } from "../../blockchain/blockchain_reader/BlockchainReader";
+import { Logger } from "../../service/logger/Logger";
+import { ToolStrategyUniswap } from "../tool/ToolStrategyUniswap";
+import { Rule, RuleParams } from "./Rule";
 // import {UNISWAPV3_POOL_ABI} from '@uniswap/v3-core/artifacts/contracts/UniswapV3Pool.sol/UniswapV3Pool.json';
 /* eslint-disable max-len */
 export interface RuleParamsUniswapPSPRebalance extends RuleParams {
-  pperTriggerThresholdPercentage: number; // trigger rebalance when we are more than (upperThreshold% * upper tick)
+  upperTriggerThresholdPercentage: number; // trigger rebalance when we are more than (upperThreshold% * upper tick)
   lowerTriggerThresholdPercentage: number; // trigger rebalance when we are less than (lowerThreshold% * lower tick)
   upperTargetTickPercentage: number; // Where we want to be after rebalance currentUniswapTick * newUpperTickPercentage = newUpperTick
   lowerTargetTickPercentage: number; // Where we want to be after rebalance currentUniswapTick * newLowerTickPercentage = newLowerTick
@@ -16,32 +16,34 @@ export interface RuleParamsUniswapPSPRebalance extends RuleParams {
 export class RuleUniswapPSPRebalance extends Rule {
   private uniswapStrategy: ToolStrategyUniswap;
   constructor(
-      logger: Logger,
-      blockchainReader: BlockchainReader,
-      ruleLabel: string,
-      params: RuleParams,
+    logger: Logger,
+    blockchainReader: BlockchainReader,
+    ruleLabel: string,
+    params: RuleParams
   ) {
     super(logger, blockchainReader, ruleLabel, params);
     this.uniswapStrategy = new ToolStrategyUniswap(
-        (params as RuleParamsUniswapPSPRebalance).strategyAddress,
-        blockchainReader,
+      (params as RuleParamsUniswapPSPRebalance).strategyAddress,
+      blockchainReader
     );
   }
   public async evaluate(): Promise<void> {
-    // check if based on the thresholds if we are in proper range then no action
     // if not calculate new lowertick and uppertick based on the currentTick
     // call rebalance function based on the new params
-    // const pool = await this.uniswapStrategy.getPoolAddress();
-    // const currentTick = await this.blockchainReader.callViewFunction(
-    //     pool,
-    //     UNISWAPV3_POOL_ABI,
-    //     'slot0',
-    // );
-    // const upperTick = await this.uniswapStrategy.upperTick();
-    // const lowerTick = await this.uniswapStrategy.lowerTick();
+    const params = this.params as RuleParamsUniswapPSPRebalance;
+    const currentTick = await this.uniswapStrategy.currentTick();
+    const upperTick = await this.uniswapStrategy.upperTick();
+    const lowerTick = await this.uniswapStrategy.lowerTick();
+    const acceptedUpperTick =
+      (params.upperTriggerThresholdPercentage * upperTick) / 100;
+    const acceptedLowerTick =
+      (params.lowerTriggerThresholdPercentage * lowerTick) / 100;
+    if (currentTick <= acceptedUpperTick || currentTick >= acceptedLowerTick) {
+      return;
+    }
   }
 
   protected generateUniqueKey(): string {
-    throw new Error('Method not implemented.');
+    throw new Error("Method not implemented.");
   }
 }
