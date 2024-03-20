bc3b9eed6f4f7a986c44f7da967b855f1c111a0a
tomerXarchimedes <tomer@archimedesfi.com>
2024-03-20 19:30:43 -0400

    update

--- scripts/container_main_groot/build_main_groot_container.sh
+++ scripts/container_main_groot/build_main_groot_container.sh
@@ -1,13 +1,8 @@
 #!/bin/bash
 
-#call: build_container.sh $ENVIRONMENT $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY $AWS_REGION
-
-AWS_ACCESS_KEY_ID=$1
-AWS_SECRET_ACCESS_KEY=$2
-
 echo "Building Groot"
 
 docker pull node:20
-docker build --no-cache --build-arg AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID --build-arg AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -t groot-container -f scripts/container_main_groot/Dockerfile .
+docker build --no-cache --build-arg -t groot-container -f scripts/container_main_groot/Dockerfile .
 
 echo "Groot container built successfully"
--- scripts/k8s/local/setup_local_env.sh
+++ scripts/k8s/local/setup_local_env.sh
@@ -7,17 +7,19 @@ kind create cluster --name groot-cluster
 kubectl create namespace groot
 
 sudo ./scripts/container_reader_node/build_read_node_container.sh $API_KEY_ALCHEMY
-sudo ./scripts/container_main_groot/build_main_groot_container.sh $AWS_ACCESS_KEY_ID $AWS_SECRET_ACCESS_KEY
+sudo ./scripts/container_main_groot/build_main_groot_container.sh
 
 # Load containers to Kind cluster
 kind load docker-image groot-container --name groot-cluster
 kind load docker-image arch-production-node:latest --name groot-cluster
+
+kubectl create secret generic aws-access-key-id --from-literal=AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+kubectl create secret generic aws-secret-access-key --from-literal=AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 
  # deploy pod
 kubectl apply -f scripts/k8s/local/demoapp-configmap.yaml
 kubectl apply -f scripts/k8s/local/deployment.yaml
 kubectl apply -f scripts/k8s/local/service.yaml
 kubectl apply -f scripts/k8s/local/cronjob.yaml
-
 
  kubectl get pods
