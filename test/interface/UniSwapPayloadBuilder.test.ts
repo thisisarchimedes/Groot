a18db4269c383873ff12a8f8f636c52d233fd45e
sacredwx <sacredwx@gmail.com>
2024-04-22 13:20:58 +0300

    [Add] UniSwapPayloadBuilder for wBTC Strategy Tests

--- test/interface/UniSwapPayloadBuilder.test.ts
+++ test/interface/UniSwapPayloadBuilder.test.ts
@@ -1,6 +1,15 @@
-import {assert} from 'chai';
+import {assert, expect} from 'chai';
 import 'dotenv/config';
 import UniSwap from '../../src/rule_engine/tool/uni_swap/UniSwap';
+import UniSwapPayloadBuilder from '../../src/rule_engine/tool/uni_swap/UniSwapPayloadBuilder';
+import {ModulesParams} from '../../src/types/ModulesParams';
+import {ConfigServiceAWS} from '../../src/service/config/ConfigServiceAWS';
+import {LoggerConsole} from '../../src/service/logger/LoggerConsole';
+import {BlockchainNodeLocal} from '../../src/blockchain/blockchain_nodes/BlockchainNodeLocal';
+import {BlockchainReader} from '../../src/blockchain/blockchain_reader/BlockchainReader';
+import {AbiFetcherEtherscan} from '../../src/rule_engine/tool/abi_repository/AbiFetcherEtherscan';
+import {AbiRepo} from '../../src/rule_engine/tool/abi_repository/AbiRepo';
+import {AbiStorageDynamoDB} from '../../src/rule_engine/tool/abi_repository/AbiStorageDynamoDB';
 
 const WETH = '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2';
 const WETH_DECIMALS = 18;
@@ -11,11 +20,44 @@ describe('UniSwap', function() {
   // eslint-disable-next-line no-invalid-this
   this.timeout(20000);
 
+  const modulesParams: ModulesParams = {};
   let uniSwap: UniSwap;
+  let uniSwapPayloadBuilder: UniSwapPayloadBuilder;
 
-  beforeEach(function() {
+  beforeEach(async function() {
     const alchemyRpcUrl: string = `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY!}`;
     uniSwap = new UniSwap(alchemyRpcUrl);
+
+    modulesParams.configService = new ConfigServiceAWS('StableApp', 'us-east-1');
+    await modulesParams.configService.refreshConfig();
+
+    // Setup Logger
+    modulesParams.logger = new LoggerConsole();
+
+    // Starting nodes
+    modulesParams.mainNode = new BlockchainNodeLocal(
+        modulesParams,
+        `http://localhost:${process.env.MAIN_LOCAL_NODE_PORT || 8545}`,
+        'localNodeAlchemy',
+    );
+    modulesParams.altNode = new BlockchainNodeLocal(
+        modulesParams,
+        `http://localhost:${process.env.ALT_LOCAL_NODE_PORT || 18545}`,
+        'localNodeInfura',
+    );
+    await Promise.all([modulesParams.mainNode.startNode(), modulesParams.altNode.startNode()]);
+
+    modulesParams.blockchainReader = new BlockchainReader(modulesParams);
+
+    const abiStorage = new AbiStorageDynamoDB(modulesParams);
+    const abiFetcher = new AbiFetcherEtherscan(modulesParams);
+    modulesParams.abiRepo = new AbiRepo(modulesParams, abiStorage, abiFetcher);
+
+    uniSwapPayloadBuilder = new UniSwapPayloadBuilder(
+        modulesParams.configService,
+        modulesParams.blockchainReader,
+        modulesParams.abiRepo,
+    );
   });
 
   it('Test payload build', async function() {
@@ -41,5 +83,29 @@ describe('UniSwap', function() {
     // Eth should be more than 0.005 (sanity)
     assert(Number(swapOutputAmount) > 0.005, 'swapOutputAmount should be greater than 0.005');
     assert(payload, expectedPayload);
+  });
+
+  it('Test payload build for open position on wBTC strategy', async function() {
+    const blockNumber = await modulesParams.blockchainReader?.getBlockNumber();
+    const blockTimestamp = await modulesParams.blockchainReader?.getBlockTimestamp(blockNumber!);
+    const payload = await uniSwapPayloadBuilder.getOpenPositionSwapPayload(
+        2n,
+        '0x7694cd972baa64018e5c6389740832e4c7f2ce9a',
+       blockTimestamp!,
+    );
+
+    expect(payload).to.be.eq('0x');
+  });
+
+  it('Test payload build for close position on wBTC strategy', async function() {
+    const blockNumber = await modulesParams.blockchainReader?.getBlockNumber();
+    const blockTimestamp = await modulesParams.blockchainReader?.getBlockTimestamp(blockNumber!);
+    const payload = await uniSwapPayloadBuilder.getClosePositionSwapPayload(
+        '0x7694cd972baa64018e5c6389740832e4c7f2ce9a',
+        2,
+       blockTimestamp!,
+    );
+
+    expect(payload).to.be.eq('0x');
   });
 });
